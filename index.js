564c26d8f597a275ab1e3a99855ebbc356f95190
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-15 16:03:09 +0100

    esercizio

---
+++ index.js
@@ -0,0 +1,21 @@
+// EX 5: COMPRARE QUALCOSA SU AMAZON
+
+        // Fare un regalo per il compleanno di Silvia
+        //  - Accendere computer / Sbloccare cellulare
+        //  - Aprire Google
+        //  - Aprire Amazon
+        //  - Cercare possibili regali
+        //  - Creare lista regali
+        //  - Scorri lista regali
+        
+        // FINCHÉ un regalo non è stato comprato
+        //  - scorri lista regali
+          
+        //    SE lampada da tavolo arriva in tempo
+        //     - compra lampada da tavolo
+    
+        //    SE stilografica rientra in budget disponibile 
+        //     - compra stilografica
+
+        //     ALTRIMENTI
+        //      -Compra Libro
